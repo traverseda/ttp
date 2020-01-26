12e0ef6b67de1905830dcf55a00a88495f70a4f6
Alex Davies <traverse.da@gmail.com>
2020-01-26 00:28:48 -0400

    Getting set up on pypi

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 setup(
     name="time-tracker-plus",
     version="1.0",
+    license='MIT'
+    author = 'Alex Davies',
+    author_email = 'traverse.da@gmail.com',
+    url = 'https://github.com/traverseda/ttp',
+    download_url = 'https://github.com/traverseda/ttp/archive/v1.0.tar.gz'
     packages=find_packages(),
     entry_points = {
         'console_scripts': ['ttp=ttp.main:cli'],
