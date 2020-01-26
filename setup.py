0a122e0aaea7e99aa36394310433c69fc966c2ab
Alex Davies <traverse.da@gmail.com>
2020-01-26 00:49:46 -0400

    Added templates and description

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="time-tracker-plus",
-    version="1.0",
-    license='MIT'
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="1.1",
+    license='MIT',
     author = 'Alex Davies',
     author_email = 'traverse.da@gmail.com',
     url = 'https://github.com/traverseda/ttp',
-    download_url = 'https://github.com/traverseda/ttp/archive/v1.0.tar.gz'
+    download_url = 'https://github.com/traverseda/ttp/archive/v1.0.tar.gz',
     packages=find_packages(),
+    package_data={
+        "": ["templates/*"],
+    },
     entry_points = {
         'console_scripts': ['ttp=ttp.main:cli'],
     },
